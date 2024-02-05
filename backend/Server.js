80471cb6f0f8191abd31ec3f9eb79277b17e329f
Mohammadsiddiqueps <siddiqueps333@gmail.com>
2024-02-05 21:47:23 +0530

    set emoji message

--- backend/Server.js
+++ backend/Server.js
@@ -15,7 +15,7 @@ const io = socketIO(server,{
     methods :['GET','POST']
   }
 });
-
+ 
 const PORT = process.env.PORT || 5000;
 const MONGO_URL = process.env.MONGO_URL;
 
--- landing/src/components/chat/Chat.jsx
+++ landing/src/components/chat/Chat.jsx
@@ -1,5 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './Chat.scss';
+import EmojiPicker from 'emoji-picker-react';
+
+// import 'emoji-mart/css/emoji-mart.css';
+import { Picker } from 'emoji-mart';
+import { Data } from 'emoji-mart';
 import { CiSearch } from "react-icons/ci";
 import { IoSend } from "react-icons/io5";
 import { MdOutlineEmojiEmotions } from "react-icons/md";
@@ -15,14 +20,15 @@ const Chat = () => {
   const [chatList, setChatList] = useState([]);
   const [message, setMessage] = useState('');
   const [messageDate, setMessageDate] = useState('');
-  const [lastDisplayedDate, setLastDisplayedDate] = useState(null);
+  const [lastDisplayedDate, setLastDisplayedDate] = useState('');
   const { mainUsername, setMainUsername } = useMainUsername();
+const [emojiOpen,setEmojiOpen]=useState(false)
+
 
   const ha = localStorage.getItem('userData');
   const userData = JSON.parse(ha);
 
-  const sendMessage = async (e) => {
-    e.preventDefault();
+  const sendMessage = async () => {
     if (!message) return;
     const today = new Date();
     const minutes = today.getMinutes() < 10 ? "0" + today.getMinutes() : today.getMinutes();
@@ -67,8 +73,9 @@ const Chat = () => {
     console.log(userData.username);
     fetchChatList();
     fetchChatMessages();
-    console.log(currentUser)
-  }, []);
+    scrollToBottom();
+    console.log(currentUser);
+  }, [currentUser]); // Updated dependency
 
   const fetchChatList = async () => {
     try {
@@ -89,8 +96,6 @@ const Chat = () => {
         const response = await axios.post('http://localhost:5000/searchchat', {
           username: searchQuery,
         });
-
-        // console.log(response.data);
 
         if (Array.isArray(response.data)) {
           setUsernameResults(response.data);
@@ -114,6 +119,30 @@ const Chat = () => {
   useEffect(() => {
     handleSearch();
   }, [searchQuery]);
+
+  useEffect(() => {
+    if (fetchedMessages.length > 0) {
+      const firstMessageDate = new Date(fetchedMessages[0].date).toLocaleDateString();
+      const lastMessageDate = new Date(fetchedMessages[fetchedMessages.length - 1].date).toLocaleDateString();
+      
+      setLastDisplayedDate(firstMessageDate);
+  
+      // Check if the last displayed date is different from the new last message date
+      if (lastDisplayedDate !== lastMessageDate) {
+        scrollToBottom();
+      }
+    }
+  }, [fetchedMessages, lastDisplayedDate]);
+  
+
+  const chatContainerRef = useRef(null);
+
+  // Function to scroll to the bottom of the chat container
+  const scrollToBottom = () => {
+    if (chatContainerRef.current) {
+      chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+    }
+  };
 
   return (
     <div>
@@ -147,24 +176,29 @@ const Chat = () => {
             ></input>
           </div>
           <div className="chat-list">
-            {chatLister.map((chat) => (
-              <div
-                key={chat.id}
-                className="chat-item"
-                onClick={() => setCurrentUser(chat)}
-              >
-                <div className="profile-circle-left">
-                  <img
-                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSofPxnJT4MLpeCDPJV85tCX7nivJRk22P5fGXq8oGX15Sy2ajQwnEV9vmckA&s"
-                    alt={`${chat.name} Profile`}
-                    className="profile-image-chat"
-                  />
-                </div>
-                <div className="chat-info">
-                  <div className="chat-name">{chat.name}</div>
-                </div>
-              </div>
-            ))}
+          {chatLister.map((chat) => (
+  // Check if chat email exists in fetchedMessages
+  userData.email !== chat.email  && (
+    <div
+      key={chat.id}
+      className="chat-item"
+      onClick={() => setCurrentUser(chat)}
+    >
+      <div className="profile-circle-left">
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSofPxnJT4MLpeCDPJV85tCX7nivJRk22P5fGXq8oGX15Sy2ajQwnEV9vmckA&s"
+          alt={`${chat.name} Profile`}
+          className="profile-image-chat"
+        />
+      </div>
+      <div className="chat-info">
+        <div className="chat-name">{chat.name}</div>
+      </div>
+    </div>
+  )
+))}
+
+
           </div>
           <div className="onlythischat">
             <h6>No more chats</h6>
@@ -181,18 +215,22 @@ const Chat = () => {
             />
             <h2 style={{ margin: '14px' }}>{currentUser.email}</h2>
           </div>
-          <div className="chat-message-display"style={currentUser === '' ? { display: 'none' } : {}}>
-  {fetchedMessages.map((message) => (
-    <div key={message._id}>
-      {(currentUser.email === message.sender || currentUser.email === message.receiver) && (
-        <div className="chat-date-display">
-          {/* Display the formatted date */}
-          <h6>{new Date(message.date).toLocaleDateString()}</h6>
-        </div>
-      )}
-      {message.sender === currentUser.email ? (
-        // Display recieve messages
-        <div className="chat-message-recieve">
+          <div className="chat-message-display"  style={currentUser === '' ? { display: 'none' } : {}} ref={chatContainerRef}>
+          {fetchedMessages.map((message) => (
+  <div key={message._id}>
+    {(currentUser.email === message.sender || currentUser.email === message.receiver) && (
+      <div>
+        {lastDisplayedDate !== new Date(message.date).toLocaleDateString() && (
+          <div className="chat-date-display">
+            <h6>{new Date(message.date).toLocaleDateString()}</h6>
+          </div>
+        )}
+      </div>
+    )}
+
+    {message.sender === currentUser.email ? (
+      // Display receive messages
+      <div className="chat-message-recieve">
         <img
           alt='profile'
           src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbIhi9l4npCGPNWMAc6szDbxp75kjB3c0R5w&s'
@@ -202,33 +240,60 @@ const Chat = () => {
           {message.content}<p className='message-time'>{message.time}</p>
         </div>
       </div>
-      ) : (
-        // Display sent messages
+    ) : message.receiver === currentUser.email ? (
+      // Display sent messages
       <div className="just">
-
-         <div className="chat-message-box-send">
-         {message.content}<p className='message-time'>{message.time}</p>
-       </div>
+        <div className="chat-message-box-send">
+          {message.content}<p className='message-time'>{message.time}</p>
+        </div>
       </div>
-      )}
-    </div>
-  ))}
+    ) : null}
+  </div>
+))}
+          </div>
+<div>
 
+              {emojiOpen && (
+              <EmojiPicker style={{ zIndex: 1000, position: 'absolute', bottom: '60px', right: '40%' }}
+              // onSelect={(emoji) => {
+              //   console.log(emoji);
+              // }}
+              onEmojiClick={
 
-</div>
+                (emoji) => {
+                  setMessage((prevMessage) => prevMessage + emoji.emoji);
+                  console.log(emoji);
+                  setEmojiOpen(!emojiOpen);
 
-
+                }              }              />
+              )}
+                </div>
           <div className="chat-message-send" style={currentUser === '' ? { display: 'none' } : {}}>
             <div className="chat-message-text">
-              <MdOutlineEmojiEmotions id="icon-in-message" />
+            <MdOutlineEmojiEmotions
+            id="icon-in-message"
+            onClick={() => {
+              setEmojiOpen(!emojiOpen);
+              console.log(emojiOpen);
+            }}
+          />
               <input
-                type="text"
-                id="input-in-message"
-                placeholder="Send some messages"
-                onChange={(e) => setMessage(e.target.value)}
-              ></input>
-            </div>
-            <IoSend id="chat-send-icon" onClick={sendMessage} />
+  type="text"
+  id="input-in-message"
+  placeholder="Send some messages"
+  value={message}  // Add this line to bind the input value to the message state
+  onChange={(e) => setMessage(e.target.value)}
+  onKeyDown={(e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault(); // Prevent default behavior (e.g., newline character)
+      sendMessage(); // Call the sendMessage function
+    }
+  }}
+/>
+</div>
+<IoSend id="chat-send-icon" onClick={sendMessage} />
+
+
           </div>
         </div>
       </div>
